16be5644dc872505ebe157d86f47708deea07c89
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-06-24 12:08:55 -0400

    Add Node class for lists

--- lib/hash-utils/node.rb
+++ lib/hash-utils/node.rb
@@ -1,1 +1,13 @@
 # frozen_string_literal: true
+
+# Defines a node in a linked list
+class Node
+  attr_reader :key, :value
+  attr_accessor :next_node
+
+  def initialize(key, value, next_node = nil)
+    @key = key
+    @value = value
+    @next_node = next_node
+  end
+end
