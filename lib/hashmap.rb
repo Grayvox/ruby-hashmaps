4208a6f10e30a383eb6c8717b2924734b0c10915
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-06-24 12:41:17 -0400

    Add basis for HashMap object

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,1 +1,13 @@
 # frozen_string_literal: true
+
+# Defines the hashmap, which contains Linked lists, which contains nodes
+class HashMap
+  attr_accessor :capacity
+
+  LOAD_FACTOR = 0.75
+
+  def initialize(capacity = 16)
+    @capacity = capacity
+    @buckets = Array.new(capacity)
+  end
+end
